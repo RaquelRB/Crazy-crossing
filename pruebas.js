9f88e9b91df2cf0a90977a79eb257887837edb18
Raquel R <raquelrobles95@gmail.com>
2020-11-01 09:56:00 +0100

    Added game background, cannons in two directions, left-right character animation, updated instructions, added level up

--- pruebas.js
+++ pruebas.js
@@ -1,11 +1,11 @@
+//------GAME AREA------//
 
-var myGameArea = {
-    canvas: document.createElement("canvas"),
+let myGameArea = {
+    canvas: document.getElementById("canvas"),
     start: function () {
         this.canvas.width = 1000;
         this.canvas.height = 500;
         this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
         this.interval = setInterval(updateGameArea, 20);
         window.addEventListener('keydown', function (e) {
@@ -20,17 +20,23 @@ var myGameArea = {
     },
     stop: function () {
         clearInterval(this.interval);
+        alert('YOU LOSE')
+    },
+    win: function (){
+        alert('LEVEL 1 completed!! Do you want to continue to level 2?')
+        startLevel2()
     }
 }
 
-function everyinterval(n) {
-    if ((myGameArea.frameNo / n) % 1 == 0) { return true; }
-    return false;
-}
 
-var myGamePiece;
-var block1, block2, block3, block4, block5, block6
-var myScore;
+//------MY VARIABLES------//
+
+let player;
+let obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6
+let myBackground;
+
+
+//------ELEMENTS------//
 
 function component(width, height, color, x, y, type) {
     this.type = type;
@@ -44,6 +50,7 @@ function component(width, height, color, x, y, type) {
     this.speedY = 0;
     this.x = x;
     this.y = y;
+
     this.update = function () {
         ctx = myGameArea.context;
         if (this.type == "text") {
@@ -57,20 +64,22 @@ function component(width, height, color, x, y, type) {
             ctx.fillRect(this.x, this.y, this.width, this.height);
         }
     }
+
     this.newPos = function () {
         this.x += this.speedX * 2;
         this.y += this.speedY * 2;
     }
+
     this.crashWith = function (otherobj) {
-        var myleft = this.x;
-        var myright = this.x + (this.width);
-        var mytop = this.y;
-        var mybottom = this.y + (this.height);
-        var otherleft = otherobj.x;
-        var otherright = otherobj.x + (otherobj.width);
-        var othertop = otherobj.y;
-        var otherbottom = otherobj.y + (otherobj.height);
-        var crash = true;
+        let myleft = this.x;
+        let myright = this.x + (this.width);
+        let mytop = this.y;
+        let mybottom = this.y + (this.height);
+        let otherleft = otherobj.x;
+        let otherright = otherobj.x + (otherobj.width);
+        let othertop = otherobj.y;
+        let otherbottom = otherobj.y + (otherobj.height);
+        let crash = true;
         if ((mybottom < othertop) ||
             (mytop > otherbottom) ||
             (myright < otherleft) ||
@@ -82,145 +91,192 @@ function component(width, height, color, x, y, type) {
 
 }
 
+
+//------FUNCTIONS------//
+
 function movePlayer() {
-    myGamePiece.speedX = 0
-    myGamePiece.speedY = 0
-    if (myGameArea.key && myGameArea.key == 37) { myGamePiece.speedX = -4; }
-    if (myGameArea.key && myGameArea.key == 39) { myGamePiece.speedX = 4; }
-    if (myGameArea.key && myGameArea.key == 38) { myGamePiece.speedY = -4; }
-    if (myGameArea.key && myGameArea.key == 40) { myGamePiece.speedY = 4; }
+    player.speedX = 0
+    player.speedY = 0
+    if (myGameArea.key && myGameArea.key == 37 && player.x > 10) { //---LEFT
+        player.speedX = -4; 
+        player.image.src = "/images/marioFlyLeft.png"
+    } 
+
+    if (myGameArea.key && myGameArea.key == 39 && player.x < 930) { //---RIGHT
+        player.speedX = 4; 
+        player.image.src = "/images/marioFlyRight.png"
+    } 
+
+    if (myGameArea.key && myGameArea.key == 38 && player.y > 10) {player.speedY = -4;} //---UP
+
+    if (myGameArea.key && myGameArea.key == 40 && player.y < 430) {player.speedY = 4; } //---DOWN
 }
 
-function moveBlocks(x) {
-    switch (x) {
-        case '1': (block1.y < 500) ? (block1.y += 5) : (block1.y = -60); break;
-        case '2': (block2.y < 500) ? (block2.y += 1) : (block2.y = -60); break;
-        case '3': (block3.y < 500) ? (block3.y += 7) : (block3.y = -60); break;
-        case '4': (block4.y < 500) ? (block4.y += 2) : (block4.y = -60); break;
-        case '5': (block5.y < 500) ? (block5.y += 4) : (block5.y = -60); break;
-        case '6': (block6.y < 500) ? (block6.y += 4) : (block6.y = -60);
+function moveObstacles(obstacleNum) {
+    switch (obstacleNum) {
+        case '1': (obstacle1.y > -60) ? (obstacle1.y -= 6) : (obstacle1.y = 540); break;
+        case '2': (obstacle2.y < 500) ? (obstacle2.y += 9) : (obstacle2.y = -60); break;
+        case '3': (obstacle3.y < 500) ? (obstacle3.y += 5) : (obstacle3.y = -60); break;
+        case '4': (obstacle4.y > -60) ? (obstacle4.y -= 7) : (obstacle4.y = 540); break;
+        case '5': (obstacle5.y < 500) ? (obstacle5.y += 6) : (obstacle5.y = -60); break;
+        case '6': (obstacle6.y < 500) ? (obstacle6.y += 9) : (obstacle6.y = -60);
     }
+}
+
+function checkCrash() {
+    if (player.crashWith(obstacle1) || player.crashWith(obstacle2) || player.crashWith(obstacle3) || player.crashWith(obstacle4) || player.crashWith(obstacle5) || player.crashWith(obstacle6)) {
+        myGameArea.stop();
+    } 
+}
+
+function checkGoal(){
+    if (player.x >= 900){
+        myGameArea.win();
+    }
+}
+
+
+//------UPDATES------//
+
+function upDateComponents(){
+    myBackground.newPos(); myBackground.update()
+    player.newPos(); player.update()
+    obstacle1.update();
+    obstacle2.update();
+    obstacle3.update();
+    obstacle4.update();
+    obstacle5.update();
+    obstacle6.update();
+}
+
+function upDatePositions (){
+    movePlayer(); 
+    moveObstacles('1')
+    moveObstacles('2')
+    moveObstacles('3')
+    moveObstacles('4')
+    moveObstacles('5')
+    moveObstacles('6')
 }
 
 function updateGameArea() {
-    if (myGamePiece.crashWith(block1) || myGamePiece.crashWith(block2) || myGamePiece.crashWith(block3) || myGamePiece.crashWith(block4) || myGamePiece.crashWith(block5) || myGamePiece.crashWith(block6)) {
-        myGameArea.stop();
-    } else {
-        myGameArea.clear();
-        myGameArea.frameNo += 1;
-        movePlayer()
-        myScore.text = "SCORE: " + myGameArea.frameNo;
-        myScore.update();
-        myGamePiece.newPos()
-        myGamePiece.update()
-
-        moveBlocks('1')
-        moveBlocks('2')
-        moveBlocks('3')
-        moveBlocks('4')
-        moveBlocks('5')
-        moveBlocks('6')
-
-        block1.update();
-        block2.update();
-        block3.update();
-        block4.update();
-        block5.update();
-        block6.update();
-    }
+    checkCrash()
+    checkGoal()
+    myGameArea.clear();
+    myGameArea.frameNo += 1; 
+    upDatePositions()
+    upDateComponents()
 }
 
+//------START GAME------//
 
 function startGame() {
     myGameArea.start();
-    myGamePiece = new component(60, 60, "red", 10, 120);
-    block1 = new component(60, 60, "brown", 125, 0);
-    block2 = new component(60, 60, "yellow", 250, 0);
-    block3 = new component(60, 60, "orange", 375, 0);
-    block4 = new component(60, 60, "grey", 500, 0);
-    block5 = new component(60, 60, "aqua", 625, 0);
-    block6 = new component(60, 60, "black", 750, 0);
-    myScore = new component("30px", "Consolas", "black", 280, 40, "text");
+    myBackground = new component(1000, 500, "/images/fondo3.png", 0, 0, "image");
+    player = new component(60, 60, "/images/marioFlyRight.png", 10, 210, "image");
+    obstacle1 = new component(60, 60, "./images/canonUp.png", 125, 400, "image");
+    obstacle2 = new component(60, 60, "./images/canonDown.png", 250, 0, "image");
+    obstacle3 = new component(60, 60, "./images/canonDown.png", 375, 0, "image");
+    obstacle4 = new component(60, 60, "./images/canonUp.png", 500, 0, "image");
+    obstacle5 = new component(60, 60, "./images/canonDown.png", 625, 0, "image");
+    obstacle6 = new component(60, 60, "./images/canonDown.png", 750, 0, "image");
 }
 
 
-startGame()
+// startGame()
+
+window.onload = () => {
+    document.getElementById('start-button').onclick = () => {
+      startGame();
+    };
+  }
 
 
-//.......FOR SPEED.....
-// function moveUp() {
-//     myGamePiece.speedY -= 1;
-// }
+  //---LEVEL 2---//
 
-// function moveDown() {
-//     myGamePiece.speedY += 1;
-// }
+  function startLevel2() {
+    startGame2()
+}
 
-// function moveLeft() {
-//     myGamePiece.speedX -= 1;
-// }
+  let myGameArea2 = {
+    canvas: document.getElementById("canvas"),
+    start: function () {
+        this.canvas.width = 1000;
+        this.canvas.height = 500;
+        this.context = this.canvas.getContext("2d");
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea2, 20);
+        window.addEventListener('keydown', function (e) {
+            myGameArea.key = e.keyCode;
+        })
+        window.addEventListener('keyup', function (e) {
+            myGameArea.key = false;
+        })
+    },
+    clear: function () {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop: function () {
+        clearInterval(this.interval);
+        alert('YOU LOSE')
+    },
+    win: function (){
+        clearInterval(this.interval)
+        alert('YOU WIN')
+    }
+}
 
-// function moveRight() {
-//     myGamePiece.speedX += 1;
-// }
+function startGame2() {
+    myGameArea2.start();
+    myBackground = new component(1000, 500, "/images/fondo3.png", 0, 0, "image");
+    player = new component(60, 60, "/images/marioFlyRight.png", 10, 210, "image");
+    obstacle1 = new component(90, 60, "./images/canonUp.png", 125, 400, "image");
+    obstacle2 = new component(60, 60, "./images/canonDown.png", 250, 0, "image");
+    obstacle3 = new component(60, 60, "./images/canonDown.png", 375, 0, "image");
+    obstacle4 = new component(60, 60, "./images/canonUp.png", 500, 0, "image");
+    obstacle5 = new component(60, 60, "./images/canonDown.png", 625, 0, "image");
+    obstacle6 = new component(60, 60, "./images/canonDown.png", 750, 0, "image");
+}
 
-// 
+function upDateComponents2(){
+    myBackground.newPos(); myBackground.update()
+    player.update()
+    obstacle1.update();
+    obstacle2.update();
+    obstacle3.update();
+    obstacle4.update();
+    obstacle5.update();
+    obstacle6.update();
+}
 
+function upDatePositions2 (){
+    movePlayer(); player.newPos()
+    moveObstacles2('1')
+    moveObstacles2('2')
+    moveObstacles2('3')
+    moveObstacles2('4')
+    moveObstacles2('5')
+    moveObstacles2('6')
+}
 
-// function movePlayer(){
-//     switch (direction) {
-//         case 'left':
-//             if (this.x <= 75){
-//                 return this.x -= 20
-//             } else {
-//                 this.x--
-//             }
-//             break;
-//         case 'right':
-//             if (this.x >= 900){
-//                 return this.x += 20
-//             }else {
-//                 this.x++ 
-//             }
-//             break;
-//         case 'up':
-//             if (this.y <= 75){
-//                 this.y -= 20
-//             } else {
-//                 this.y--
-//             }
-//             break;
-//         case 'down':
-//             if (this.y >= 500){
-//                 this.y -= 20
-//             } else {
-//                 this.y++
-//             }
-//         default:
-//             throw new Error('Invalid direction')
-//     }
-//     }
+function moveObstacles2(obstacleNum) {
+    switch (obstacleNum) {
+        case '1': (obstacle1.y > -60) ? (obstacle1.y -= 7) : (obstacle1.y = 540); break;
+        case '2': (obstacle2.y < 500) ? (obstacle2.y += 9) : (obstacle2.y = -60); break;
+        case '3': (obstacle3.y < 500) ? (obstacle3.y += 6) : (obstacle3.y = -60); break;
+        case '4': (obstacle4.y > -60) ? (obstacle4.y -= 8) : (obstacle4.y = 540); break;
+        case '5': (obstacle5.y < 500) ? (obstacle5.y += 7) : (obstacle5.y = -60); break;
+        case '6': (obstacle6.y < 500) ? (obstacle6.y += 9) : (obstacle6.y = -60);
+    }
+}
 
-
-// document.addEventListener('keydown', (event) => {
-//     switch (event.key) {
-//         case 'ArrowLeft':
-//             movePlayer('left')
-//             break;
-//         case 'ArrowRight':
-//             movePlayer('right')
-//             break;
-//         case 'ArrowUp':
-//             movePlayer('up')
-//             break;
-//         case 'ArrowDown':
-//             movePlayer('down')
-//     }
-// });
-
-
-
-
-
+function updateGameArea2() {
+    checkCrash()
+    checkGoal()
+    myGameArea2.clear();
+    myGameArea2.frameNo += 1; 
+    upDatePositions2()
+    upDateComponents2()
+}
 
 
--- src/game.js
+++ src/game.js
@@ -6,7 +6,6 @@ let myGameArea = {
         this.canvas.width = 1000;
         this.canvas.height = 500;
         this.context = this.canvas.getContext("2d");
-        // document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
         this.interval = setInterval(updateGameArea, 20);
         window.addEventListener('keydown', function (e) {
@@ -21,22 +20,22 @@ let myGameArea = {
     },
     stop: function () {
         clearInterval(this.interval);
-        alert('GAME OVER')
+        alert('YOU LOSE')
     },
-    win: function (){
-        clearInterval(this.interval)
-        alert('YOU WIN')
+    levelUp: function (){
+        // alert('LEVEL 1 completed!! Do you want to continue to level 2?')
+        clearInterval(this.interval);
+        this.clear()
+        startLevel2()
     }
 }
 
 
 //------MY VARIABLES------//
 
-let myGamePiece;
-let block1, block2, block3, block4, block5, block6
-// let myScore;
+let player;
+let obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6
 let myBackground;
-let gameOver;
 
 
 //------ELEMENTS------//
@@ -53,7 +52,7 @@ function component(width, height, color, x, y, type) {
     this.speedY = 0;
     this.x = x;
     this.y = y;
-    
+
     this.update = function () {
         ctx = myGameArea.context;
         if (this.type == "text") {
@@ -67,10 +66,12 @@ function component(width, height, color, x, y, type) {
             ctx.fillRect(this.x, this.y, this.width, this.height);
         }
     }
+
     this.newPos = function () {
         this.x += this.speedX * 2;
         this.y += this.speedY * 2;
     }
+
     this.crashWith = function (otherobj) {
         let myleft = this.x;
         let myright = this.x + (this.width);
@@ -94,85 +95,95 @@ function component(width, height, color, x, y, type) {
 
 
 //------FUNCTIONS------//
+
 function movePlayer() {
-    myGamePiece.speedX = 0
-    myGamePiece.speedY = 0
-    if (myGameArea.key && myGameArea.key == 37 && myGamePiece.x > 10) { myGamePiece.speedX = -4; }
+    player.speedX = 0
+    player.speedY = 0
+    if (myGameArea.key && myGameArea.key == 37 && player.x > 10) { //---LEFT
+        player.speedX = -4; 
+        player.image.src = "/images/marioFlyLeft.png"
+    } 
 
-    if (myGameArea.key && myGameArea.key == 39 && myGamePiece.x < 930) { myGamePiece.speedX = 4; }
+    if (myGameArea.key && myGameArea.key == 39 && player.x < 930) { //---RIGHT
+        player.speedX = 4; 
+        player.image.src = "/images/marioFlyRight.png"
+    } 
 
-    if (myGameArea.key && myGameArea.key == 38 && myGamePiece.y > 10) { myGamePiece.speedY = -4; }
+    if (myGameArea.key && myGameArea.key == 38 && player.y > 10) {player.speedY = -4;} //---UP
 
-    if (myGameArea.key && myGameArea.key == 40 && myGamePiece.y < 430) { myGamePiece.speedY = 4; }
+    if (myGameArea.key && myGameArea.key == 40 && player.y < 430) {player.speedY = 4; } //---DOWN
 }
 
-function moveBlocks(blockNum) {
-    switch (blockNum) {
-        case '1': (block1.y < 500) ? (block1.y += 5) : (block1.y = -60); break;
-        case '2': (block2.y < 500) ? (block2.y += 1) : (block2.y = -60); break;
-        case '3': (block3.y < 500) ? (block3.y += 7) : (block3.y = -60); break;
-        case '4': (block4.y < 500) ? (block4.y += 2) : (block4.y = -60); break;
-        case '5': (block5.y < 500) ? (block5.y += 4) : (block5.y = -60); break;
-        case '6': (block6.y < 500) ? (block6.y += 8) : (block6.y = -60);
+function moveObstacles(obstacleNum) {
+    switch (obstacleNum) {
+        case '1': (obstacle1.y > -60) ? (obstacle1.y -= 6) : (obstacle1.y = 540); break;
+        case '2': (obstacle2.y < 500) ? (obstacle2.y += 9) : (obstacle2.y = -60); break;
+        case '3': (obstacle3.y < 500) ? (obstacle3.y += 5) : (obstacle3.y = -60); break;
+        case '4': (obstacle4.y > -60) ? (obstacle4.y -= 7) : (obstacle4.y = 540); break;
+        case '5': (obstacle5.y < 500) ? (obstacle5.y += 6) : (obstacle5.y = -60); break;
+        case '6': (obstacle6.y < 500) ? (obstacle6.y += 9) : (obstacle6.y = -60);
     }
 }
 
-function checkObstacles() {
-    if (myGamePiece.crashWith(block1) || myGamePiece.crashWith(block2) || myGamePiece.crashWith(block3) || myGamePiece.crashWith(block4) || myGamePiece.crashWith(block5) || myGamePiece.crashWith(block6)) {
+function checkCrash() {
+    if (player.crashWith(obstacle1) || player.crashWith(obstacle2) || player.crashWith(obstacle3) || player.crashWith(obstacle4) || player.crashWith(obstacle5) || player.crashWith(obstacle6)) {
         myGameArea.stop();
     } 
 }
 
-function checkWinGame(){
-    if (myGamePiece.x >= 900){
-        myGameArea.win();
+function checkGoal(){
+    if (player.x >= 900){
+        myGameArea.levelUp();
     }
 }
 
+
 //------UPDATES------//
+
+function upDateComponents(){
+    myBackground.newPos(); myBackground.update()
+    player.newPos(); player.update()
+    obstacle1.update();
+    obstacle2.update();
+    obstacle3.update();
+    obstacle4.update();
+    obstacle5.update();
+    obstacle6.update();
+}
+
+function upDatePositions (){
+    movePlayer(); 
+    moveObstacles('1')
+    moveObstacles('2')
+    moveObstacles('3')
+    moveObstacles('4')
+    moveObstacles('5')
+    moveObstacles('6')
+}
+
 function updateGameArea() {
-
-    checkObstacles()
-    checkWinGame()
-
+    checkCrash()
+    checkGoal()
     myGameArea.clear();
-    myGameArea.frameNo += 1;
-    
-    movePlayer()
-    moveBlocks('1')
-    moveBlocks('2')
-    moveBlocks('3')
-    moveBlocks('4')
-    moveBlocks('5')
-    moveBlocks('6')
-    
-    myGamePiece.newPos()
-    // checkWinGame()
-    myBackground.update()
-    // myScore.text = "SCORE: " + myGameArea.frameNo;
-    // myScore.update();
-    myGamePiece.update()
-    block1.update();
-    block2.update();
-    block3.update();
-    block4.update();
-    block5.update();
-    block6.update();
+    myGameArea.frameNo += 1; 
+    upDatePositions()
+    upDateComponents()
 }
 
 //------START GAME------//
+
 function startGame() {
     myGameArea.start();
-    myBackground = new component(1000, 500, "#72F59D", 0, 0)
-    myGamePiece = new component(60, 60, "./images/yoshi.png", 10, 210, "image");
-    block1 = new component(60, 60, "./images/cañon.png", 125, 0, "image");
-    block2 = new component(60, 60, "./images/cañon.png", 250, 0, "image");
-    block3 = new component(60, 60, "./images/cañon.png", 375, 0, "image");
-    block4 = new component(60, 60, "./images/cañon.png", 500, 0, "image");
-    block5 = new component(60, 60, "./images/cañon.png", 625, 0, "image");
-    block6 = new component(60, 60, "./images/cañon.png", 750, 0, "image");
-    // myScore = new component("30px", "Consolas", "black", 800, 40, "text");
+    myBackground = new component(1000, 500, "/images/fondo3.png", 0, 0, "image");
+    player = new component(60, 60, "/images/marioFlyRight.png", 10, 210, "image");
+    obstacle1 = new component(60, 60, "./images/canonL1Up.png", 125, 400, "image");
+    obstacle2 = new component(60, 60, "./images/canonL1Down.png", 250, 0, "image");
+    obstacle3 = new component(60, 60, "./images/canonL1Down.png", 375, 0, "image");
+    obstacle4 = new component(60, 60, "./images/canonL1Up.png", 500, 0, "image");
+    obstacle5 = new component(60, 60, "./images/canonL1Down.png", 625, 0, "image");
+    obstacle6 = new component(60, 60, "./images/canonL1Down.png", 750, 0, "image");
 }
+
 
 // startGame()
 
@@ -181,6 +192,104 @@ window.onload = () => {
       startGame();
     };
   }
+
+
+  //------------------------------LEVEL 2--------------------------------//
+
+
+
+  //------GAME AREA - Level 2------//
+
+  let myGameArea2 = {
+    canvas: document.getElementById("canvas"),
+    start: function () {
+        this.canvas.width = 1000;
+        this.canvas.height = 500;
+        this.context = this.canvas.getContext("2d");
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea2, 20);
+        window.addEventListener('keydown', function (e) {
+            myGameArea.key = e.keyCode;
+        })
+        window.addEventListener('keyup', function (e) {
+            myGameArea.key = false;
+        })
+    },
+    clear: function () {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop: function () {
+        clearInterval(this.interval);
+        alert('YOU LOSE')
+    },
+    win: function (){
+        clearInterval(this.interval)
+        alert('YOU WIN')
+    }
+}
+
+//------UPDATES - Level 2------//
+
+function upDateComponents2(){
+    myBackground.newPos(); myBackground.update()
+    player.newPos()
+    player.update()
+    obstacle1.update();
+    obstacle2.update();
+    obstacle3.update();
+    obstacle4.update();
+    obstacle5.update();
+    obstacle6.update();
+}
+
+function upDatePositions2 (){
+    movePlayer(); 
+    moveObstacles2('1')
+    moveObstacles2('2')
+    moveObstacles2('3')
+    moveObstacles2('4')
+    moveObstacles2('5')
+    moveObstacles2('6')
+}
+
+function moveObstacles2(obstacleNum) {
+    switch (obstacleNum) {
+        case '1': (obstacle1.y > -60) ? (obstacle1.y -= 7) : (obstacle1.y = 540); break;
+        case '2': (obstacle2.y < 500) ? (obstacle2.y += 10) : (obstacle2.y = -60); break;
+        case '3': (obstacle3.y < 500) ? (obstacle3.y += 6) : (obstacle3.y = -60); break;
+        case '4': (obstacle4.y > -60) ? (obstacle4.y -= 8) : (obstacle4.y = 540); break;
+        case '5': (obstacle5.y < 500) ? (obstacle5.y += 7) : (obstacle5.y = -60); break;
+        case '6': (obstacle6.y < 500) ? (obstacle6.y += 10) : (obstacle6.y = -60);
+    }
+}
+
+function checkGoal2(){
+    if (player.x >= 900){
+        myGameArea.win();
+    }
+}
+
+function updateGameArea2() {
+    checkCrash()
+    checkGoal2()
+    myGameArea2.clear();
+    myGameArea2.frameNo += 1; 
+    upDatePositions2()
+    upDateComponents2()
+}
+
+
+function startLevel2() {
+    myGameArea2.start();
+    myBackground = new component(1000, 500, "/images/fondo3.Level2.png", 0, 0, "image");
+    player = new component(60, 60, "/images/marioFlyRight.png", 10, 210, "image");
+    obstacle1 = new component(60, 60, "./images/canonL2Up.png", 125, 400, "image");
+    obstacle2 = new component(60, 60, "./images/canonL2Down.png", 250, 0, "image");
+    obstacle3 = new component(60, 60, "./images/canonL2Down.png", 375, 0, "image");
+    obstacle4 = new component(60, 60, "./images/canonL2Up.png", 500, 0, "image");
+    obstacle5 = new component(60, 60, "./images/canonL2Down.png", 625, 0, "image");
+    obstacle6 = new component(60, 60, "./images/canonL2Down.png", 750, 0, "image");
+}
 
 
 
